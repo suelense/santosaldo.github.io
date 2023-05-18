0fbe8d150da35f5a242b77fe2b99e37dcb9723e2
suelense <suelense@gmail.com>
2023-05-18 14:36:17 -0300

    criação grid

---
+++ script.js
@@ -0,0 +1,7 @@
+/*const dataCotacao = document.getElementById('data').onsubmit
+const valor = document.getElementById('valor').onsubmit*/
+
+function conversao() {
+    /* https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='05-02-2023'&$top=100&$format=json*/
+    return console.log('heloo')
+}
