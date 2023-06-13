d4d1a808884c8cd507c944d06054f125d4ea2e72
suelense <suelense@gmail.com>
2023-06-13 16:33:01 -0300

    Alteracao CSS e JS

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ function validaData() {
     var dataFormatada = dataConsulta.split('-').join('/');
     var dataInicial = '1984/12/01';
     if (dataFormatada < dataInicial){
-        alert("Cálculo disponíveis a partir de 01/12/1984. Informe uma nova data");
-        stop} else {
+        alert("Cálculo disponíveis a partir de 01/12/1984. Informe uma nova data");} else {
             return dataConsulta;
         }
     }
@@ -13,14 +12,14 @@ function formatarURL(dia) {
     var data = validaData();
     if (data.slice(5,7) == '01') {
         var mes = 12;
-        ano = data.slice(0,4) - 1;
+        var ano = data.slice(0,4) - 1;
     } else {
         if (data.slice(5,7) < 11){
-            mes = "0" + String(data.slice(5,7) - 1);
+            var mes = "0" + String(data.slice(5,7) - 1);
         } else {
-            mes = data.slice(5,7) -1;
+            var mes = data.slice(5,7) -1;
         }
-        ano = data.slice(0,4);
+        var ano = data.slice(0,4);
     }
     var dataURL= mes+"-"+dia+"-"+ano;
     var url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='"+dataURL+"'&$top=100&$format=json"    
@@ -39,9 +38,9 @@ function conversao() {
         var cotacao = valores.cotacaoCompra;
         var valor = document.getElementById('valor').value;
         var conversao = cotacao * valor;
-        var mensagem = "<p>Cotação do dia " + data.slice(8,10) + "/" + data.slice(5,7) + "/" + data.slice(0,4) + ": R$ " + cotacao + "<br>Cálculo: US$ " + valor + " * " + cotacao + " = R$ " + conversao.toFixed(2) + "<br><br><h4> O valor a ser informado no carnê-leão é R$ " + conversao.toFixed(2) + "</h4>";
+        var mensagem = "<p>Cotação do dia " + data.slice(8,10) + "/" + data.slice(5,7) + "/" + data.slice(0,4) + ": R$ " + cotacao + "<br>Cálculo: US$ " + valor + " * " + cotacao + " = R$ " + conversao.toFixed(2) + "<br><br></p>" + 
+        "<h4> O valor a ser informado no carnê-leão é R$ " + conversao.toFixed(2) + "</h4>";
         document.getElementById("resultado").innerHTML=mensagem;
     }
-    var valores = request.send();
-    console.log(valores)
+    request.send();
 }
