99f5fa82662fc623f93a292259a176b4f00f98bd
suelense <suelense@gmail.com>
2023-11-20 21:06:11 -0300

    menu

--- script.js
+++
@@ -1,8 +0,0 @@
-function menu() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "none") {
-      x.style.display = "inline-block";
-    } else {
-      x.style.display = "none";
-    }
-}
